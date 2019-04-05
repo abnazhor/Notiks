2ffa16c331af19a076ddc567e2720cb52db7e7f1
Abnazhor <jorbar982@outlook.com>
2019-04-05 11:52:10 +0200

    Cambiado código y añadido diagrama BBDD

--- Código/routes/index.js
+++ Código/routes/index.js
@@ -2,7 +2,7 @@ let fs = require("fs");
 
 //Esta función realiza la importación de los archivos en los que se encuentran las distintas rutas
 //de express para modularizar el proyecto y hacerlo más legible y modificable.
-module.exports = function(app) {
+module.exports = (app) => {
     fs.readdirSync(__dirname).forEach((file) => {
         if (file == "index.js") return; //Omitimos el índice ya que es este el que lee los ficheros.
         if (file == "error.js") return; //Omitimos los errores y los dejamos para el final.
