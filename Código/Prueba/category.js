195f4979348e7f31f16323c4417edd542dd7ca7e
Abnazhor <jorbar982@outlook.com>
2019-04-26 13:45:54 +0200

    Actualización del código

---
+++ Código/Prueba/category.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.delete("/category/:id", (req, res) => {
+
+    });
+
+    app.post("/category/:id", (req, res) => {
+
+    });
+
+    app.get("/category/:id", (req, res) => {
+
+    });
+}
---
+++ Código/Prueba/note.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.get("/note/:id", (req, res) => {
+
+    });
+
+    app.delete("/note/:id", (req, res) => {
+
+    });
+
+    app.post("/note/:id", (req, res) => {
+
+    });
+}
