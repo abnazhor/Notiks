46e37b5c46105f42b10c16350702fe1969225f4c
Abnazhor <jorbar982@outlook.com>
2019-05-17 00:23:10 +0200

    Añadido SQL con datos de prueba.

---
+++ Código/INSTALLATION/dummy_data/tables-10-05-2019-18-23-beta.sql
@@ -0,0 +1,41 @@
+-- Generation time: Fri, 10 May 2019 18:23:17 +0000
+-- Host: mysql.hostinger.ro
+-- DB name: u574849695_22
+/*!40030 SET NAMES UTF8 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `tables`;
+CREATE TABLE `tables` (
+  `user_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `table_id` int(11) NOT NULL,
+  `title` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`table_id`),
+  KEY `fk_tables_users` (`user_id`),
+  CONSTRAINT `fk_tables_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`email`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `tables` VALUES ('alek81@example.org','1','Similique cupiditate qui cum quo un'),
+('adelle.ratke@example.org','2','Officia aut vitae culpa consequatur'),
+('agustin56@example.org','3','Voluptatem enim quas iste voluptate'),
+('agutkowski@example.net','4','Accusamus consectetur dolorem tempo'),
+('abe.quigley@example.org','6','Voluptatem enim expedita quas incid'); 
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
